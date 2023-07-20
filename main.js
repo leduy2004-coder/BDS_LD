4e8d76345891168bf8edd97cd6b3eca6fea6b5b3
leduy2004-coder <leduy8762@gmail.com>
2023-07-20 09:06:55 +0700

    new

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ let icon = document.querySelector('.menu-icon');
 let menu = document.querySelector('.menu');
 let modal = document.querySelector('.modal');
 let menuList = document.querySelectorAll('.menu li');
+let modalInput = document.querySelector('.modal-input');
+let formLogin = document.querySelector('.form-login');
+let formRegister = document.querySelector('.form-register');
+let formBtnBack = document.querySelectorAll('.back');
+let registerBtn = document.querySelector('.register-btn');
+let loginBtn = document.querySelector('.login-btn');
+let openLogin = document.querySelector('.open-login');
+let openRegister = document.querySelector('.open-register');
 
 icon.addEventListener("click", function(e) {
     modal.classList.add("open");
@@ -37,3 +45,42 @@ prev.addEventListener("click", function() {
 })
 
 
+
+loginBtn.addEventListener("click", function() {
+    modalInput.classList.add("block");
+    formLogin.classList.add("block");
+})
+
+registerBtn.addEventListener("click", function() {
+    formRegister.classList.add("block");
+    modalInput.classList.add("block");
+})
+
+
+modalInput.addEventListener("click", function(e) {
+    if (e.target == e.currentTarget) {
+        modalInput.classList.remove("block");
+        formRegister.classList.remove("block");
+        formLogin.classList.remove("block");
+    }
+})
+
+formBtnBack.forEach((item) => {
+    item.addEventListener("click", function() {
+        modalInput.classList.remove("block");
+       formRegister.classList.remove("block");
+       formLogin.classList.remove("block");
+    })
+});
+
+openLogin.addEventListener("click", function() {
+    formLogin.classList.add("block");
+    formRegister.classList.remove("block");
+    modalInput.classList.add("block");
+})
+
+openRegister.addEventListener("click", function() {
+    formRegister.classList.add("block");
+    formLogin.classList.remove("block");
+    modalInput.classList.add("block");
+})
